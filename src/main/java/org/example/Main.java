033b03f6ff38f7e5f77e866a7303cd8da18a568d
kaurilves <kaurilves@gmail.com>
2022-10-17 10:12:41 +0300

    WIP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,13 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("jdbc:mysql://localhost:3306/quizdb", "root", "password");
-        try {
-            Connection connection = dcm.getConnection();
-            QuestionDaoImpl questionDaoImpl = new QuestionDaoImpl(connection);
+        QuestionDao qd = new QuestionDao();
+        Question question = new Question("Mis su nim on", 2, 2);
+        qd.create(question);
 
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/org/example/QuestionDao.java
+++ src/main/java/org/example/QuestionDao.java
@@ -1,25 +1,117 @@
 package org.example;
 
-
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public abstract class QuestionDao<Question> {
+public class QuestionDao {
 
-    protected final Connection connection;
+    public static Connection connection;
+    private static final String INSERT = "INSERT into questions(NAME,DIFFICULTY_RANK,TOPICS_ID) VALUES(?,?,?)";
+    private static final String GET_ONE = "SELECT ID,NAME,DIFFICULTY_RANK,TOPICS_ID FROM questions WHERE ID=?";
+    private static final String UPDATE = "UPDATE questions SET NAME =?, DIFFICULTY_RANK=?, TOPICS_ID=? WHERE ID=?";
+    private static final String DELETE = "DELETE FROM questions WHERE ID=?";
+    private static final String GET_LIST_BY_TOPIC = "SELECT * FROM questions WHERE TOPICS_ID=?";
 
-    public abstract void create(Question question);
 
-    public abstract Question findById(Integer id);
+    public QuestionDao(){
+        if(connection == null){
+            String dbUrl = "jdbc:mysql://localhost:3306/quizdb";
+            try {
+                connection = DriverManager.getConnection (dbUrl, "root", "password");
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-    public abstract void update(Question question);
+    public void create(Question question) {
 
-    public abstract void delete(Integer id);
+        try{
+            PreparedStatement ps = connection.prepareStatement(INSERT);
+            ps.setString(1, question.getName());
+            ps.setInt(2, question.getDifficultyRank());
+            ps.setInt(3, question.getTopicId());
 
-    public abstract List<Question> findAllByTopicId(Integer topicId);
+            ps.executeUpdate();
 
-    protected QuestionDao(Connection connection) {
-        this.connection = connection;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Question findById (Integer id) {
+        Question question = null;
+        try {
+            PreparedStatement ps = connection.prepareStatement(GET_ONE);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                question = new Question();
+                question.setId(rs.getInt("ID"));
+                question.setName(rs.getString("NAME"));
+                question.setDifficultyRank(rs.getInt("DIFFICULTY_RANK"));
+                question.setTopicId(rs.getInt("TOPICS_ID"));
+
+            }
+        }  catch(Exception e) {
+            e.printStackTrace();
+        }
+        return question;
+    }
+
+    public void update(Question question) {
+
+        try{
+            PreparedStatement ps = connection.prepareStatement(UPDATE);
+            ps.setString(1, question.getName());
+            ps.setInt(2, question.getDifficultyRank());
+            ps.setInt(3, question.getTopicId());
+            ps.setInt(4, question.getId());
+            ps.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void delete(Integer id) {
+        try{
+            PreparedStatement ps = connection.prepareStatement(DELETE);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Question> findAllByTopicId(Integer topicId) {
+        List<Question> questions = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(GET_LIST_BY_TOPIC);
+            ps.setInt(1, topicId);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Question question = new Question();
+                question.setId(rs.getInt("ID"));
+                question.setName(rs.getString("NAME"));
+                question.setDifficultyRank(rs.getInt("DIFFICULTY_RANK"));
+                question.setTopicId(rs.getInt("TOPICS_ID"));
+                questions.add(question);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return questions;
     }
 }
+
+
+
