6be1e69d346ce30a2d67571494b776358244f584
kaurilves <kaurilves@gmail.com>
2022-10-17 10:19:18 +0300

    WIP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,5 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        QuestionDao qd = new QuestionDao();
-        Question question = new Question("Mis su nim on", 2, 2);
-        qd.create(question);
-
     }
 }
